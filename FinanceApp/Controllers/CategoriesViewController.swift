d0ae92496b5ac82d7ecd04457a0eea3bddc66b7a
DanikGuun <80112309+DanikGuun@users.noreply.github.com>
2024-06-11 21:53:24 +0300

    Скелет для CategoryView

---
+++ FinanceApp/Controllers/CategoriesViewController.swift
@@ -0,0 +1,15 @@
+//
+//  CategoriesViewController.swift
+//  FinanceApp
+//
+//  Created by Данила Бондарь on 11.06.2024.
+//
+
+import Foundation
+import UIKit
+
+class CategoriesViewController: UIViewController{
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
