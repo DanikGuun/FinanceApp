7e4764ff7c6c30927a45747dc5e16984a1e81ec1
DanikGuun <80112309+DanikGuun@users.noreply.github.com>
2024-06-14 15:41:45 +0500

    Добавил кнопку больше для цветов

--- FinanceApp/Controllers/CategoryHandlerViewController.swift
+++ FinanceApp/Controllers/CategoryHandlerViewController.swift
@@ -8,7 +8,7 @@
 import Foundation
 import UIKit
 
-class CategoryHandlerViewController: UIViewController, ColorPickCircleDelegate{
+class CategoryHandlerViewController: UIViewController, ColorPickCircleDelegate, UICollectionViewDelegate, UICollectionViewDataSource{
 
     @IBOutlet weak var menuBackground: UIView!
     @IBOutlet weak var colorPickerStack: UIStackView!
@@ -28,13 +28,35 @@ class CategoryHandlerViewController: UIViewController, ColorPickCircleDelegate{
             let colorPick = ColorPickCircle(color: c, frame: CGRect(x: 0, y: 0, width: height, height: height), delegate: self)
             colorPickerStack.addArrangedSubview(colorPick)
         }
-        stackWidthConstraint.constant = CGFloat(colors.count)*(height)
+        stackWidthConstraint.constant = CGFloat(colors.count + 1)*(height)
+        
+        //MoreButton
+        let more = UIImageView(frame: CGRect(x: 0, y: 0, width: height, height: 100))
+        more.image = UIImage(systemName: "ellipsis.circle.fill")
+        more.tintColor = UIColor(cgColor: CGColor(red: 224/255, green: 224/255, blue: 224/255, alpha: 1))
+        more.isUserInteractionEnabled = true
+        let recogniser = UITapGestureRecognizer(target: self, action: #selector(moreColorsPressed(_:)))
+        more.addGestureRecognizer(recogniser)
+        colorPickerStack.addArrangedSubview(more)
+    }
+    @objc func moreColorsPressed(_ sender: UIImage){
+        print(3)
     }
     func colorPicked(color: ColorPickCircle) {
-        for item in colorPickerStack.arrangedSubviews as! [ColorPickCircle]{
+        for item in (colorPickerStack.arrangedSubviews as! [ColorPickCircle]).dropLast(){
             item.layer.borderWidth = 0
         }
         color.layer.borderWidth = 3
-        color.layer.borderColor = CGColor(red: 102/255, green: 102/255, blue: 102/255, alpha: 1)
+        color.layer.borderColor = UIColor(named: "ColorPicked")?.cgColor
     }
+    
+    // MARK: Collection View
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        <#code#>
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        <#code#>
+    }
+    
 }
