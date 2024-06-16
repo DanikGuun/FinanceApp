f11ef515ec2a44012ea4213fad1fe093cfb595c4
DanikGuun <80112309+DanikGuun@users.noreply.github.com>
2024-06-17 01:32:59 +0500

    Перевел мульти-выбор цветов на UIViewController

--- FinanceApp/Controllers/Category/MultiColorpickerViewController.swift
+++ FinanceApp/Controllers/Category/MultiColorpickerViewController.swift
@@ -8,24 +8,29 @@
 import Foundation
 import UIKit
 
-class MultiColorpickerViewController: UICollectionViewController{
+class MultiColorpickerViewController: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource{
+    
+    @IBOutlet weak var colorsCollection: UICollectionView!
+    @IBOutlet weak var menuBackground: UIView!
     
     var parentController: MultiColorpickerParent!
     let colors: [UIColor] = Model.shared.getAvailableColors()
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        colorsCollection.dataSource = self
+        colorsCollection.delegate = self
+        Appereances.applyMenuBorder(&menuBackground)
     }
-    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
         return colors.count
     }
-    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "multiColorPicker", for: indexPath) as! MultiColorpickerCell
         cell.setup(color: colors[indexPath.item])
         return cell
     }
-    override func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
         let cell = collectionView.cellForItem(at: indexPath) as! MultiColorpickerCell
         parentController.colorPickedFromMultiMenu(color: cell.color)
         navigationController?.popViewController(animated: true)
